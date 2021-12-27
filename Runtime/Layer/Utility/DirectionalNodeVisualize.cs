4707b39afc50b67cf1704f96846ee592895e869b
GrahamMueller(Desktop) <GrahamMueller1992@gmail.com>
2021-12-26 21:36:07 -0800

    Added visualization object with tests.  This can be used to visualize a directional layer's nodes.

--- Runtime/Layer/Utility/DirectionalNodeVisualize.cs
+++ Runtime/Layer/Utility/DirectionalNodeVisualize.cs
@@ -1,153 +1,232 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+//Add 'multiply block'
+
+//Set background base per tile (this tile is red, this tile is blue)
+//  Color background!
+//  Set background tile!
 
 namespace DirectionalPathingLayers
 {
     class DirectionalNodeVisualize
     {
-        Color backgroundColor;
 
         Color[] directionalNodeColors;
 
-        Bitmap bitmap;
+        //Bitmap bitmap;
 
         int tileSize;
-        public DirectionalNodeVisualize(int tileSize)
+
+        Color[,] backgroundTileColor;
+        Color[,] foregroundColors;
+        /// <summary>
+        /// 
+        /// </summary>
+        public void SetBackgroundTileColor(int indexX, int indexY, Color setColor)
+        {
+            this.backgroundTileColor[indexX, indexY] = setColor;
+        }
+
+        //public DirectionalNodeVisualize(DirectionalNodes[,] nodes, int tileSize)
+        //{
+        //    this.tileSize = tileSize;
+
+        //    this.directionalNodeColors = new Color[6] { Color.Red, Color.Red, Color.Green, Color.Green, Color.Blue, Color.Blue };
+
+        //    this.backgroundTileColor = new Color[tileSize, tileSize];
+        //    for (int x = 0; x < this.backgroundTileColor.GetLength(0); ++x)
+        //    {
+        //        for (int y = 0; y < this.backgroundTileColor.GetLength(1); ++y)
+        //        {
+        //            this.SetBackgroundTileColor(x, y, Color.White);
+        //        }
+        //    }
+        //}
+
+        public DirectionalNodeVisualize(DirectionalNode[,] nodes, int tileSize, Color backgroundColor, Color[] directionColors)
         {
             this.tileSize = tileSize;
+
+            this.directionalNodeColors = directionColors;
+
+            this.backgroundTileColor = new Color[nodes.GetLength(0), nodes.GetLength(1)];
+            for (int x = 0; x < this.backgroundTileColor.GetLength(0); ++x)
+            {
+                for (int y = 0; y < this.backgroundTileColor.GetLength(1); ++y)
+                {
+                    this.SetBackgroundTileColor(x, y, backgroundColor);
+                }
+            }
+
+            this.foregroundColors = new Color[nodes.GetLength(0) * tileSize, nodes.GetLength(1) * tileSize];
+            for (int x = 0; x < this.foregroundColors.GetLength(0); ++x)
+            {
+                for (int y = 0; y < this.foregroundColors.GetLength(1); ++y)
+                {
+                    this.foregroundColors[x, y] = Color.Empty;
+                }
+            }
+            this.ApplyNodes(nodes);
+
+        }
+
+        public DirectionalNodeVisualize(DirectionalNode[,] nodes, int tileSize)
+        {
+            this.tileSize = tileSize;
+
+            this.directionalNodeColors = new Color[6] { Color.Red, Color.Red, Color.Green, Color.Green, Color.Blue, Color.Blue };
+
+            this.backgroundTileColor = new Color[nodes.GetLength(0), nodes.GetLength(1)];
+            for (int x = 0; x < this.backgroundTileColor.GetLength(0); ++x)
+            {
+                for (int y = 0; y < this.backgroundTileColor.GetLength(1); ++y)
+                {
+                    this.SetBackgroundTileColor(x, y, Color.White);
+                }
+            }
+
+            this.foregroundColors = new Color[nodes.GetLength(0) * tileSize, nodes.GetLength(1) * tileSize];
+            for (int x = 0; x < this.foregroundColors.GetLength(0); ++x)
+            {
+                for (int y = 0; y < this.foregroundColors.GetLength(1); ++y)
+                {
+                    this.foregroundColors[x, y] = Color.Empty;
+                }
+            }
+            this.ApplyNodes(nodes);
+        }
+
+        public Bitmap GetBitmap()
+        {
+            Bitmap bitmap = new Bitmap(this.foregroundColors.GetLength(0), this.foregroundColors.GetLength(1));
+
+            //Set background colors
+            for (int x = 0; x < this.backgroundTileColor.GetLength(0); ++x)
+            {
+                for (int y = 0; y < this.backgroundTileColor.GetLength(1); ++y)
+                {
+                    for (int x_block = 0; x_block < this.tileSize; ++x_block)
+                    {
+                        for (int y_block = 0; y_block < this.tileSize; ++y_block)
+                        {
+                            bitmap.SetPixel(x * this.tileSize + x_block, y * this.tileSize + y_block, this.backgroundTileColor[x, y]);
+                        }
+                    }
+                }
+            }
+
+            for (int x = 0; x < this.foregroundColors.GetLength(0); ++x)
+            {
+                for (int y = 0; y < this.foregroundColors.GetLength(1); ++y)
+                {
+                    if (this.foregroundColors[x, y] != Color.Empty)
+                    {
+                        bitmap.SetPixel(x, this.foregroundColors.GetLength(1) - 1 - y, this.foregroundColors[x, y]);
+                    }
+                }
+            }
+
+            return bitmap;
+        }
+
+        public void SaveAsPng(string fileName)
+        {
+            Bitmap bitmap = this.GetBitmap();
+
+            bitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
         }
 
         /// <summary>
         /// Creates a nefwf visual representation of nodes.  
         /// </summary>
         /// <param name="nodes"></param>
-        public void ApplyNodes(DirectionalNode[,] nodes)
+        void ApplyNodes(DirectionalNode[,] nodes)
         {
-            this.bitmap = this.GetBlankBitmap(nodes.GetLength(0), nodes.GetLength(1));
-
             for (int x = 0; x < nodes.GetLength(0); ++x)
             {
                 for (int y = 0; y < nodes.GetLength(1); ++y)
                 {
-                    Color[,] nodeColors = this.GetNodeColors(nodes[x, y]);
-
-                    //Set returned pixels in bitmap
-                    for (int x_node = 0; x_node < nodeColors.GetLength(0); ++x_node)
-                    {
-                        for (int y_node = 0; y_node < nodeColors.GetLength(1); ++y_node)
-                        {
-                            this.bitmap.SetPixel(x * this.tileSize + x_node, y * this.tileSize + y_node, nodeColors[x_node, y_node]);
-                        } //y_node
-                    } //x_node
+                    this.SetNodeColors(nodes[x, y], x, y);
                 } //y
             } //x
         }
 
-        private Bitmap GetBlankBitmap(int nodesWidth, int nodesHeight)
+        /// <summary> Returns a 2D color image of an individual node. </summary>
+        void SetNodeColors(DirectionalNode node, int indexX, int indexY)
         {
-            Bitmap bitmap = new Bitmap(nodesWidth * this.tileSize, nodesHeight * this.tileSize);
-            for (int x = 0; x < bitmap.Width; ++x)
+            int baseOffsetX = indexX * this.tileSize;
+            int baseOffsetY = indexY * this.tileSize;
+
+            //Handle creating small lines 
+            if (node.Forward != 0)
             {
-                for (int y = 0; y < bitmap.Height; ++y)
-                {
-                    this.bitmap.SetPixel(x, y, this.backgroundColor);
-                }
+                this.DrawLine(baseOffsetX + this.tileSize / 2, baseOffsetY + this.tileSize / 2,
+                              baseOffsetX + this.tileSize / 2, baseOffsetY + this.tileSize,
+                              this.directionalNodeColors[0]);
             }
-            return bitmap;
+
+            if (node.Backward != 0)
+            {
+                this.DrawLine(baseOffsetX + this.tileSize / 2, baseOffsetY + this.tileSize / 2,
+                              baseOffsetX + this.tileSize / 2, baseOffsetY,
+                              this.directionalNodeColors[1]);
+            }
+
+            if (node.Left != 0)
+            {
+                this.DrawLine(baseOffsetX + this.tileSize / 2, baseOffsetY + this.tileSize / 2,
+                              baseOffsetX, baseOffsetY + this.tileSize / 2,
+                              this.directionalNodeColors[2]);
+
+            }
+
+            if (node.Right != 0)
+            {
+                this.DrawLine(baseOffsetX + this.tileSize / 2, baseOffsetY + this.tileSize / 2,
+                              baseOffsetX + this.tileSize, baseOffsetY + this.tileSize / 2,
+                              this.directionalNodeColors[3]);
+
+            }
+
+            if (node.Up != 0)
+            {
+                this.DrawLine(baseOffsetX + this.tileSize / 2, baseOffsetY + this.tileSize / 2,
+                              baseOffsetX + (int)(this.tileSize), baseOffsetY + (int)(this.tileSize),
+                              this.directionalNodeColors[4]);
+
+            }
+
+            if (node.Down != 0)
+            {
+                this.DrawLine(baseOffsetX + this.tileSize / 2, baseOffsetY + this.tileSize / 2,
+                              baseOffsetX + (int)(this.tileSize * 0.75f), baseOffsetY + (int)(this.tileSize * 0.25f),
+                              this.directionalNodeColors[4]);
+            }
+
+            this.foregroundColors[baseOffsetX + this.tileSize / 2, baseOffsetY + this.tileSize / 2] = Color.Black;
         }
 
-        /// <summary> Returns a 2D color image of an individual node. </summary>
-        Color[,] GetNodeColors(DirectionalNode node)
+        void DrawLine(int startX, int startY, int endX, int endY, Color lineColor)
         {
-            Color[,] nodeColors = new Color[this.tileSize, this.tileSize];
+            int dirWidth = endX - startX;
+            int dirHeight = endY - startY;
 
-            //Set background
-            for (int x = 0; x < nodeColors.GetLength(0); ++x)
+            //Iterate number of points needed, which will be the longer width.
+            int pointIterations = (Math.Abs(dirWidth) > Math.Abs(dirHeight)) ? Math.Abs(dirWidth) : Math.Abs(dirHeight);
+            for (int i = 0; i < pointIterations; ++i)
             {
-                for (int y = 0; y < nodeColors.GetLength(1); ++y)
+                int currentX = startX + (int)(dirWidth * i / pointIterations);
+                int currentY = startY + (int)(dirHeight * i / pointIterations);
+                if (currentX >= this.foregroundColors.GetLength(0))
                 {
-                    nodeColors[x, y] = this.backgroundColor;
+                    ;
                 }
-            }
-
-            //Create small lines indicating direction.
-            int widthTop = nodeColors.GetLength(0);
-            int widthCenter = widthTop / 2;
-
-            int heightTop = nodeColors.GetLength(1);
-            int heightCenter = heightTop / 2;
-
-            //Forward
-            if (node.directions[0] != 0)
-            {
-                int x = widthCenter;
-                for (int y = heightCenter; y < heightTop; ++y)
+                if (currentY >= this.foregroundColors.GetLength(1))
                 {
-                    nodeColors[x, y] = this.directionalNodeColors[0];
+                    ;
                 }
-
+                this.foregroundColors[currentX, currentY] = lineColor;
             }
-
-            //Back
-            if (node.directions[1] != 0)
-            {
-                int x = widthCenter;
-                for (int y = 0; y < heightCenter; ++y)
-                {
-                    nodeColors[x, y] = this.directionalNodeColors[1];
-                }
-
-            }
-
-            //Left
-            if (node.directions[2] != 0)
-            {
-                int y = heightCenter;
-                for (int x = 0; x < widthCenter; ++x)
-                {
-                    nodeColors[x, y] = this.directionalNodeColors[2];
-                }
-
-            }
-
-            //Right
-            if (node.directions[3] != 0)
-            {
-                int y = heightCenter;
-                for (int x = widthCenter; x < widthTop; ++x)
-                {
-                    nodeColors[x, y] = this.directionalNodeColors[3];
-                }
-
-            }
-
-            //Up
-            if (node.directions[4] != 0)
-            {
-                for (int x = widthCenter; x < widthTop; ++x)
-                {
-                    for (int y = heightCenter; y < heightTop; ++y)
-                    {
-                        nodeColors[x, y] = this.directionalNodeColors[4];
-                    }
-                }
-
-            }
-
-            //Down
-            if (node.directions[5] != 0)
-            {
-                for (int x = widthCenter; x < widthTop; ++x)
-                {
-                    for (int y = heightTop; y >= 0; ++y)
-                    {
-                        nodeColors[x, y] = this.directionalNodeColors[5];
-                    }
-                }
-
-            }
-
-            return nodeColors;
-
         }
     }
 }
--- Tests/NUnit/Layer/Utility/test_DirectionalNodeVisualize.cs
+++ Tests/NUnit/Layer/Utility/test_DirectionalNodeVisualize.cs
@@ -1,22 +1,141 @@
 ﻿using NUnit.Framework;
-using System.Collections.Generic;
-using System;
-
+using System.Drawing;
+using System.IO;
 
 namespace DirectionalPathingLayers.Tests.NUnit.Layer.Utility
 {
     class test_DirectionalNodeVisualize
     {
+        bool updateFiles = false;
+
         [Test]
-        public void Test_Background()
+        public void Test_Tiles()
         {
-            //throw new Exception();
+            string projectRoot = this.GetRootProjectFolder();
+            string testImage_5 = Path.GetFullPath(Path.Combine(projectRoot, @"Tests\NUnit\Layer\Utility\visualize_tiles_5.png"));
+            string testImage_10 = Path.GetFullPath(Path.Combine(projectRoot, @"Tests\NUnit\Layer\Utility\visualize_tiles_10.png"));
+
+            MapDirectionalLayer mapLayer = new MapDirectionalLayer(6, 6, new DirectionalNode(new int[] { 1, 1, 1, 1, 1, 1 }));
+            DirectionalNodeVisualize vis = new DirectionalNodeVisualize(mapLayer.DirectionLayer.directionalNodes, 5);
+            if (this.updateFiles)
+            {
+                vis.SaveAsPng(testImage_5);
+            }
+            else
+            {
+                Bitmap loadBitmap = new Bitmap(testImage_5);
+                Assert.IsTrue(this.CompareBitmaps(loadBitmap, vis.GetBitmap()));
+            }
+
+            //Test larger size.
+            vis = new DirectionalNodeVisualize(mapLayer.DirectionLayer.directionalNodes, 10);
+            if (this.updateFiles)
+            {
+                vis.SaveAsPng(testImage_10);
+            }
+            else
+            {
+                Bitmap loadBitmap = new Bitmap(testImage_10);
+                Assert.IsTrue(this.CompareBitmaps(loadBitmap, vis.GetBitmap()));
+            }
         }
 
+
+        //Set other directions to empty, set working direction to color, test for color
         [Test]
         public void Test_Directions()
         {
-            //throw new Exception();
+            string projectRoot = this.GetRootProjectFolder();
+            string testImage = Path.GetFullPath(Path.Combine(projectRoot, @"Tests\NUnit\Layer\Utility\visualize_directions.png"));
+
+
+            MapDirectionalLayer mapLayer = new MapDirectionalLayer(6, 6, new DirectionalNode(new int[] { 1, 1, 1, 1, 1, 1 }));
+            Color[] lineColors = new Color[6] { Color.Black, Color.White, Color.Red, Color.Blue, Color.Teal, Color.Aqua };
+            DirectionalNodeVisualize vis = new DirectionalNodeVisualize(mapLayer.DirectionLayer.directionalNodes, 10, Color.Orange, lineColors);
+            if (this.updateFiles)
+            {
+                vis.SaveAsPng(testImage);
+            }
+            else
+            {
+                Bitmap loadBitmap = new Bitmap(testImage);
+                Assert.IsTrue(this.CompareBitmaps(loadBitmap, vis.GetBitmap()));
+            }
         }
+
+
+        //Set background tiles
+        [Test]
+        public void Test_Background()
+        {
+            string projectRoot = this.GetRootProjectFolder();
+            string testImage = Path.GetFullPath(Path.Combine(projectRoot, @"Tests\NUnit\Layer\Utility\visualize_background.png"));
+
+            MapDirectionalLayer mapLayer = new MapDirectionalLayer(6, 6, new DirectionalNode(new int[] { 0, 1, 0, 1, 0, 1 }));
+            DirectionalNodeVisualize vis = new DirectionalNodeVisualize(mapLayer.DirectionLayer.directionalNodes, 10);
+
+            for (int x = 0; x < mapLayer.DirectionLayer.directionalNodes.GetLength(0); x+=2)
+            {
+                for (int y = 0; y < mapLayer.DirectionLayer.directionalNodes.GetLength(1); y+=2)
+                {
+                    vis.SetBackgroundTileColor(x, y, Color.Red);
+                }
+            }
+
+            if (this.updateFiles)
+            {
+                vis.SaveAsPng(testImage);
+            }
+            else
+            {
+                Bitmap loadBitmap = new Bitmap(testImage);
+                Assert.IsTrue(this.CompareBitmaps(loadBitmap, vis.GetBitmap()));
+            }
+        }
+
+        //Hacky way to work backwards from wherever the DLL for tests ends up.
+        string GetRootProjectFolder()
+        {
+            string cwd = Directory.GetCurrentDirectory();
+
+            while (!this.IsProjectRoot(cwd))
+            {
+                cwd = Path.GetFullPath(Path.Combine(cwd, @"..\"));
+            }
+
+            return cwd;
+        }
+
+
+        bool IsProjectRoot(string path)
+        {
+            string[] projectRootFolders = new string[] { "Tests", "Runtime" };
+
+            foreach (string checkFolder in projectRootFolders)
+            {
+                DirectoryInfo di = new DirectoryInfo(path);
+                DirectoryInfo[] directories = di.GetDirectories(checkFolder, SearchOption.TopDirectoryOnly);
+                if (directories.Length == 0) { return false; }
+            }
+
+            return true;
+        }
+
+
+        bool CompareBitmaps(Bitmap a, Bitmap b)
+        {
+            if (a.Width != b.Width || a.Height != b.Height) { return false; }
+
+            for (int x = 0; x < a.Width; ++x)
+            {
+                for (int y = 0; y < a.Height; ++y)
+                {
+                    if (a.GetPixel(x, y) != b.GetPixel(x, y)) { return false; }
+                }
+            }
+            return true;
+        }
+
+
     }
 }
