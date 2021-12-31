fc73a556daa9faac0b11ba7d2020230144955100
GrahamMueller(Desktop) <GrahamMueller1992@gmail.com>
2021-12-30 20:48:55 -0800

    Added benchmarking code for basic tests.

---
+++ Tests/Profiling Tests/profile_Benchmark.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+namespace DirectionalPathingLayers.Tests.Profiling_Tests
+{
+    public static class profile_Benchmark
+    {
+        static bool writeToFile = false;
+        static string outputFile = "benchmarks.log";
+        static string outputFile_run;
+        static profile_Benchmark()
+        {
+            //Setup files once per entire test run.
+            if (writeToFile)
+            {
+                System.IO.File.WriteAllText(outputFile, string.Empty);
+
+                string timeNow = DateTime.Now.ToString("yyyyMMddTHHmmss");
+                outputFile_run = timeNow + "_" + outputFile;
+            }
+        }
+
+        //https://stackoverflow.com/questions/1622440/benchmarking-method-calls-in-c-sharp
+        public static void Benchmark(Action act, int iterations)
+        {
+            //High process priority to remove variance between runs.
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+
+            //JIT - run it a couple times before measuring.
+            WarmupMethod(act, iterations);
+
+            //Ensure garbage collection has completed.
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            
+            //Perform benchmark
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < iterations; i++)
+            {
+                act.Invoke();
+            }
+            sw.Stop();
+
+            float timePerRun = (1.0f * sw.ElapsedMilliseconds) / iterations;
+            string timePerRunUnit = "mS";
+            if (timePerRun < 0.00001) //nS
+            {
+                timePerRun *= 1000 * 1000;
+                timePerRunUnit = "nS";
+            }
+            else if (timePerRun < 0.01) //uS
+            {
+                timePerRun *= 1000;
+                timePerRunUnit = "uS";
+            }
+
+            string totalTime = sw.ElapsedMilliseconds.ToString();
+            string writeLine = $"{act.Method.Name} | #{iterations}, {totalTime} mS | {timePerRun} {timePerRunUnit}";
+            Console.WriteLine(writeLine);
+            if (writeToFile)
+            {
+                WriteLineToDebugFiles(writeLine);
+            }
+
+        }
+
+        private static void WarmupMethod(Action act, int iterations)
+        {
+            
+            for (int i = 0; i < (iterations * 0.25); ++i)
+            {
+                act.Invoke(); // run once outside of loop to avoid initialization costs
+            }
+        }
+
+        static void WriteLineToDebugFiles(string outputLine)
+        {
+            using (StreamWriter streamWriter = File.AppendText(outputFile))
+            {
+                streamWriter.WriteLine(outputLine);
+
+            }
+
+            using (StreamWriter streamWriter = File.AppendText(outputFile_run))
+            {
+                streamWriter.WriteLine(outputLine);
+
+            }
+        }
+    }
+}
---
+++ Tests/Profiling Tests/profile_DirectionalLayer.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace DirectionalPathingLayers.Tests.Profiling_Tests
+{
+    class profile_DirectionalLayer
+    {
+        static int dummyInt = 0; //By existing and being used in tests, we prevent optimizations removing test code.
+
+        static DirectionalNode simpleNode; //Common node to avoid constructing when not neccesary
+
+        //Test speed of direction access
+        [Test]
+        public void test_Directions()
+        {
+            simpleNode = new DirectionalNode();
+            dummyInt = 0;
+            profile_Benchmark.Benchmark(this.profile_Directions, 100000000);
+        }
+        public void profile_Directions()
+        {
+            if (profile_DirectionalLayer.simpleNode.Forward == 0 &&
+                profile_DirectionalLayer.simpleNode.Backward == 0 &&
+                profile_DirectionalLayer.simpleNode.Left == 0 &&
+                profile_DirectionalLayer.simpleNode.Right == 0 &&
+                profile_DirectionalLayer.simpleNode.Up == 0 &&
+                profile_DirectionalLayer.simpleNode.Down == 0)
+            {
+                dummyInt++;
+            }
+        }
+
+        //Test speed of construction
+        [Test]
+        public void test_Constructor()
+        {
+            dummyInt = 0;
+            profile_Benchmark.Benchmark(this.profile_Constructor, 100000000);
+        }
+        public void profile_Constructor()
+        {
+            profile_DirectionalLayer.simpleNode = new DirectionalNode();
+        }
+    }
+
+
+}
